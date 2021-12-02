cc2f9fa0442d41a709008c3dd428b27eebe66250
Serin Anoopraj <serin.anoopraj@capgemini.com>
2021-12-02 09:51:26 +0530

    updated commit

--- GitToJenkin.java
+++ GitToJenkin.java
@@ -5,6 +5,11 @@ public class GitToJenkin {
 	System.out.println("This is my jenkin project");
 
 	System.out.println("This is second attempt");
+	
+	System.out.println("This is the updated jenkin project");
+
+	System.out.println("This is my  attempt to update java file in git");
+	
 	}
 
 }
